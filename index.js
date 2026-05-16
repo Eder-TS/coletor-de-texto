856c38e55a5ace78b7a04fcaae236dbcc59cddff
Eder-TS <steder20@yahoo.com.br>
2026-05-16 16:23:03 -0300

    Feat: Logged with help of codegen.

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
+const { loadEnvFile } = require('node:process');
 const assert = require('node:assert');
 const { chromium } = require('playwright');
 
 (async () => {
-    const browser = await chromium.launch({headless: false, slowMo: 50});
+    loadEnvFile('./.env');
+
+    const browser = await chromium.launch({headless: false, slowMo: 2000});
     const page = await browser.newPage();
     
     await page.goto('https://app.itsdnc.com.br/login');
+
+    const email = process.env.EMAIL;
+    const password = process.env.PASSWORD;
+    await page.getByLabel(/email/i).fill(email);
+    await page.getByRole('textbox', { name: 'Senha' }).fill(password);
+    await page.getByRole('button', { name: 'Entrar' }).click();
 
     await browser.close();
 })();
