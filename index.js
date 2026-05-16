f4baef66c9eef67454fc9263a36e6347de157e92
Eder-TS <steder20@yahoo.com.br>
2026-05-16 18:10:55 -0300

    Feat: Navigate and find content.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { chromium } = require('playwright');
 (async () => {
     loadEnvFile('./.env');
 
-    const browser = await chromium.launch({headless: false, slowMo: 2000});
+    const browser = await chromium.launch({headless: false, slowMo: 200});
     const page = await browser.newPage();
     
     await page.goto('https://app.itsdnc.com.br/login');
@@ -15,6 +15,14 @@ const { chromium } = require('playwright');
     await page.getByLabel(/email/i).fill(email);
     await page.getByRole('textbox', { name: 'Senha' }).fill(password);
     await page.getByRole('button', { name: 'Entrar' }).click();
+
+    await page.getByRole('link', { name: 'Cursos' }).click();
+    await page.getByRole('heading', { name: 'Engenheiro de Software' }).click();
+    await page.getByRole('button', { name: 'MATÉRIA 1 1. Introdução ao' }).click();
+    await page.getByRole('button', { name: '2. Introdução à Tecnologia' }).click();
+    await page.getByRole('tab', { name: 'Resumo' }).click();
+    
+    const content = await page.getByRole('tabpanel').innerHTML();
 
     await browser.close();
 })();
