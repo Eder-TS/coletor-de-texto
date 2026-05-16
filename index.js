78c608eaea8dd181ec7f1a8768e7f5bca568baad
Eder-TS <steder20@yahoo.com.br>
2026-05-16 12:34:45 -0300

    Feat: Access to the page.

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-const init = async ({page}) => {
+const assert = require('node:assert');
+const { chromium } = require('playwright');
+
+(async () => {
+    const browser = await chromium.launch({headless: false, slowMo: 50});
+    const page = await browser.newPage();
+    
     await page.goto('https://app.itsdnc.com.br/login');
-}
+
+    await browser.close();
+})();
