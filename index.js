b0151ccf4d9be8249f325f0ef86a293ce400b305
Eder-TS <steder20@yahoo.com.br>
2026-05-20 02:04:02 -0300

    Feat: It have iteracted over all classes of one discipline.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const { loadEnvFile } = require('node:process');
 const { chromium } = require('playwright');
-const { fs, access } = require('node:fs');
+const fs = require('node:fs');
 
-let pathToSave = '/home/ederts/Documentos/Cursos de Programação/DNC/Aula em Texto/by Coletor de Texto';
-let content;
+let pathToSave = '/home/ederts/Documentos/Cursos de Programação/DNC/Aula em Texto/by Coletor de Texto/';
+let actualClasses = 1;
+let content = 1;
+let classTitle;
 
 (async () => {
     loadEnvFile('./.env');
@@ -19,12 +21,17 @@ let content;
     await page.getByRole('textbox', { name: 'Senha' }).fill(password);
     await page.getByRole('button', { name: 'Entrar' }).click();
 
-    await page.getByRole('link', { name: 'Cursos' }).click();
-    await page.getByRole('heading', { name: 'Engenheiro de Software' }).click();
+    while (content !== 0) {
+        await page.getByRole('link', { name: 'Cursos' }).click();
+        await page.getByRole('heading', { name: 'Engenheiro de Software' }).click();
 
-    content = await iteratorToContent(page);
-    
-    //saver(content);
+        if (content === 1) {
+            content = await iteratorToContent(page);
+        } else {
+            content = await iteratorToContent(page);
+            saver(content);
+        }
+    }
 
     console.log('All content was collected!');
     await browser.close();
@@ -32,22 +39,36 @@ let content;
 
 async function iteratorToContent(page) {
     const discipline = /MATÉRIA 1 \b/i;
-    const classes = /\b1. \b/;
+    const classes = new RegExp(`\\b${actualClasses}. \\b`);
+    //console.log();
 
     await page.getByRole('button', { name: discipline }).click();
-    await page.getByRole('button').locator('span').getByText(classes).click();
     try {
-        await page.getByRole('tab', { name: 'Resumo' }).click({timeout: 250});
-        content = await page.getByRole('tabpanel').innerHTML();
-        return content;
+        await page.getByRole('button').locator('span').getByText(classes).click({timeout: 250});
+        console.log('entrou na aula')
+        return content = await internalIterator(page);
     } catch (error) {
-        console.log(error);
+        console.log(`Fim das aulas da ${discipline}`);
         return content = 0;
     }
 }
 
+async function internalIterator(page) {
+    try {
+        await page.getByRole('tab', { name: 'Resumo' }).click({timeout: 250});
+        content = await page.getByRole('tabpanel').innerHTML();
+console.log('copiou conteúdo ')
+        classTitle = `Aula ${actualClasses}`;
+        actualClasses = actualClasses + 1;
+        return content;
+    } catch (TimeoutError) {
+        console.log('pulou aula')
+        actualClasses = actualClasses + 1;
+        return content = 1;
+    }
+}
+
 function saver(content) {
-    
-    fs.writeFile(pathToSave, content, { flag: 'w+' }, err => {});
+    fs.writeFileSync(`${pathToSave}${classTitle}`, content, { flag: 'w+' }, err => {});
     return;
 }
